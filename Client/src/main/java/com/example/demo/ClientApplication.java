a2ca1cf9f30c08c1bfa211a80a03479dd958a6de
xiaobaobao007 <932824098@qq.com>
2019-07-16 19:29:24 +0800

    第一次提交项目：feign实现的负载均衡

--- Client/src/main/java/com/example/demo/ClientApplication.java
+++ Client/src/main/java/com/example/demo/ClientApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+
 public class ClientApplication {
 
     public static void main(String[] args) {
